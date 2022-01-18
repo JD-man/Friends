b1b77fc6017eba47794e2ad001c0fa2b34dd1b9d
JD-man <ekgkzmtm@gmail.com>
2022-01-19 00:03:50 +0900

    Project First Commit.

---
+++ FriendWithSameHobby/Views/PhoneAuthViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PhoneAuthViewController.swift
+//  FriendWithSameHobby
+//
+//  Created by JD_MacMini on 2022/01/18.
+//
+
+import UIKit
+
+class PhoneAuthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}
