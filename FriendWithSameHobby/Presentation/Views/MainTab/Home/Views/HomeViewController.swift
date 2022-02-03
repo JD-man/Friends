8c7cff1c0918d0469226b67c994fb764cf7d70e1
JD-man <ekgkzmtm@gmail.com>
2022-02-03 23:51:12 +0900

    config HomeVC map view.

--- FriendWithSameHobby/Presentation/Views/MainTab/Home/Views/HomeViewController.swift
+++ FriendWithSameHobby/Presentation/Views/MainTab/Home/Views/HomeViewController.swift
@@ -6,14 +6,18 @@
 //
 
 import UIKit
+import NMapsMap
 
 class HomeViewController: UIViewController {
 
     weak var coordinator: HomeCoordinator?
     
+    let mapView = NMFMapView()
+    
     init(coordinator: HomeCoordinator) {
         super.init(nibName: nil, bundle: nil)
         self.coordinator = coordinator
+        viewConfig()
     }
     
     required init?(coder: NSCoder) {
@@ -23,5 +27,10 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemGreen
+    }
+    
+    private func viewConfig() {
+        view.addSubview(mapView)
+        mapView.frame = view.bounds
     }
 }
---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'FriendWithSameHobby' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for FriendWithSameHobby
+  pod 'NMapsMap'
+
+  target 'FriendWithSameHobbyTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'FriendWithSameHobbyUITests' do
+    # Pods for testing
+  end
+
+end
